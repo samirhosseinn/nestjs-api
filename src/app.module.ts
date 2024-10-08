376bbce222d946351ad75d8f498346ff8bb27484
samirhosseinnn <sahosseini1382@gmail.com>
2024-10-08 20:22:21 +0330

    create user ORM model

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-// import { UserModule } from './user/user.module';
+import { UserModule } from './user/user.module';
 import { DatabaseModule } from 'src/database/database.module';
 import { ConfigModule } from '@nestjs/config';
 
@@ -8,7 +8,7 @@ import { ConfigModule } from '@nestjs/config';
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     DatabaseModule,
-    // UserModule,
+    UserModule,
   ]
 })
 export class AppModule { }
---
+++ src/user/entities/user.entity.ts
@@ -0,0 +1,17 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+
+@Entity({ name: "users" })
+export class User {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({ type: 'varchar', length: 255, nullable: true })
+    name: string | null;
+
+    @Column({ type: 'varchar', length: 255, unique: true, nullable: false })
+    email: string;
+
+    @Column({ type: 'varchar', length: 255, nullable: false })
+    password_hash: string;
+}
